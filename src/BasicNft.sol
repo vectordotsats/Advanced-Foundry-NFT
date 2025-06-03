063600cb7cb128c1ccc556a42c7a76365f887230
Vector <vector02ozo@gmail.com>
2025-06-03 03:27:15 +0100

    initial commit

---
+++ src/BasicNft.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    constructor() ERC721("Spiky", "SPK") {}
+}
