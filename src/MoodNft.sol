363aa55bc85f581fc19990d8208ec64de7a70c43
Vector <vector02ozo@gmail.com>
2025-06-17 00:25:59 +0100

    updated commit

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -6,15 +6,26 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MoodNft is ERC721 {
     uint256 private _tokenCounter;
-    string private _sadSvg;
-    string private _happySvg;
+    string private _sadSvgToImageURI;
+    string private _happySvgToImageURI;
 
     constructor(
-        string memory sadSvg,
-        string memory happySvg
+        string memory sadSvgToImageURI,
+        string memory happySvgToImageURI
     ) ERC721("Mood NFT", "MOOD") {
         _tokenCounter = 0;
-        _sadSvg = sadSvg;
-        _happySvg = happySvg;
+        _sadSvgToImageURI = sadSvgToImageURI;
+        _happySvgToImageURI = happySvgToImageURI;
+    }
+
+    function mintSadNft() public {
+        _safeMint(msg.sender, _tokenCounter);
+        _tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory metadata = string.concat('{"name": '", name(), "'"}')
     }
 }
